b1db39919da8275c401189499a651e3fbea6e4be
duynk190077 <duypro10x123@gmail.com>
2022-05-26 22:03:53 +0700

    fix/update-profile

---
+++ src/interfaces/Parent.ts
@@ -0,0 +1,20 @@
+export default interface Parent {
+    id?: string;
+    fullName: string;
+    yearofBirth: Number | '';
+    job: string;
+    phoneNumber: string;
+}
+
+export const defaultParent: Parent = {
+    fullName: '',
+    yearofBirth: '',
+    job: '',
+    phoneNumber: ''
+}
+
+export interface ParentField {
+    name: string;
+    field: keyof Parent;
+    editable: boolean;
+}

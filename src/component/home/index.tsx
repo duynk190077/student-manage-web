22b9c19fd44d226a3f90c1aa43db69061cf702ce
duynk190077 <duypro10x123@gmail.com>
2023-03-26 11:36:21 +0700

    continue refactor code

--- src/component/home/index.tsx
+++ src/component/home/index.tsx
@@ -1,5 +1,5 @@
-export { default as DetailPersonal } from './DetailPersonal copy';
-export { default as DetailStudent } from './DetailStudent copy';
+export { default as DetailPersonal } from './DetailPersonal';
+export { default as DetailStudent } from './DetailStudent';
 export { default as Home } from './Home';
 export { default as StudentHome } from './StudentHome';
 export { default as TeacherHome } from './TeacherHome';
